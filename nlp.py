5f30b1d6b5c7bf0fb780fdacb8fbed32704afaee
Diane10 <46956585+Diane10@users.noreply.github.com>
2021-02-04 22:44:57 +0200

    Update nlp.py

--- nlp.py
+++ nlp.py
@@ -1,3 +1,5 @@
+
+
 import pandas as pd
 import numpy as np
 from textblob import Word
@@ -6,137 +8,93 @@ import nltk
 import nltk
 nltk.download('punkt')
 import streamlit as st
-import nltk
-from nltk.corpus import stopwords
-from nltk.stem import WordNetLemmatizer
+# Importing the libraries
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+from apyori import apriori
+from mlxtend.frequent_patterns import apriori
+from mlxtend.frequent_patterns import association_rules
+from mlxtend.preprocessing import TransactionEncoder
+from mlxtend.frequent_patterns import apriori, association_rules
+
+
 nltk.download("wordnet")
 nltk.download("brown")
-nltk.download('averaged_perceptron_tagger') 
-from nltk.corpus import wordnet 
 from sumy.parsers.plaintext import PlaintextParser
 from sumy.nlp.tokenizers import Tokenizer
 from sumy.summarizers.lex_rank import LexRankSummarizer
-from nltk.stem.porter import PorterStemmer
-#Nlp
+
 st.title("Natural Language Processing with Streamlit")
 def sumy_summarize(docx):
-    parser = PlaintextParser.from_string(docx,Tokenizer("english"))
-    lex_summarizer = LexRankSummarizer()
-    summary = lex_summarizer(parser.document,3)
-    summary_list = [str(sentence) for sentence in summary]
-    result = ' '.join(summary_list)
+    parser= PlaintextParser.from_string(docx,Tokenizer("english"))
+    lex_summarize=LexRankSummarizer(parser.document,3)
+    summary_list=[str(sentence) for sentence in summary]
+    result=' '.join(summary_list)
     return result
+
+if st.sidebar.checkbox("Market Basket Anlysis"):
+    dataset = pd.read_csv('GroceryStoreDataSet.csv')
+    dataset = list(dataset["Transaction"].apply(lambda x:x.split(',')))
+    st.write(dataset.head())
+    te = TransactionEncoder()
+    te_data = te.fit(dataset).transform(dataset)
+    df = pd.DataFrame(te_data,columns=te.columns_)
+    df = df.replace(True,1) 
+    df = df.astype(int)
+    st.write(df)
     
-def pos_tagger(nltk_tag): 
-    if nltk_tag.startswith('J'): 
-        return wordnet.ADJ 
-    elif nltk_tag.startswith('V'): 
-        return wordnet.VERB 
-    elif nltk_tag.startswith('N'): 
-        return wordnet.NOUN 
-    elif nltk_tag.startswith('R'): 
-        return wordnet.ADV 
-    else:           
-        return None    
+    freq_items = apriori(df,min_support=0.10,use_colnames=True) # Support Values
     
-message =st.text_area("Enter text")
-blob = TextBlob(message)
+    st.write(freq_items.sort_values(by = "support" , ascending = False))
+    
+    rules = association_rules(freq_items, metric="lift", min_threshold=1)
+    st.write(rules.head())
+    
+    df1 = association_rules(freq_items, metric = "confidence", min_threshold = 0.02)
+    
+    st.write(df1.head())
+    
+    st.write(df1[(df1.confidence > 0.8) & (df1.lift > 1)].sort_values(by="lift", ascending=False))
+
 if st.sidebar.checkbox("NLP"):
+    st.text_area("Enter text")
+    blob = TextBlob(text1)
     if st.checkbox('Noun phrases'):
-        if st.button("Analyse",key="1"):
-#              text1 =st.text_area("Enter text")
-             blob = TextBlob(message)
-             st.write(blob.noun_phrases)
+        st.write(blob.noun_phrases)
     if st.checkbox("show sentiment analysis"):
-#         st.subheader("analyse your text")
-#         message=st.text_area("Enter your text")  
-        if st.button("Analyse",key="2"):
-            blob = TextBlob(message)
+        st.subheader("analyse your text")
+        message=st.text_area("Enter your text")  
+        if st.button("Analyse"):
+            blob = TextBlob(text1)
             result_sentiment= blob.sentiment
             st.success(result_sentiment)
             polarity = blob.polarity
             subjectivity = blob.subjectivity
             st.write(polarity, subjectivity)
-    if st.checkbox("show words"): 
-        if st.button("Analyse",key="3"):
-            blob = TextBlob(message)
-            st.write (blob.words)
-    if st.checkbox("show sentence"):
-        if st.button("Analyse"):
-            blob = TextBlob(message)
-            st.write(blob.sentences)
-    if st.checkbox("Tokenize sentence"): 
-        if st.button("Analyse",key="4"):
-            list2 = nltk.word_tokenize(message) 
-            st.write(list2) 
-    if st.checkbox("POS tag "): 
-        if st.button("Analyse",key="5"):
-            pos_tagged = nltk.pos_tag(nltk.word_tokenize(message))   
-            st.write(pos_tagged) 
-            
-#     if st.checkbox("Removing stopword"): 
-
-#         if st.button("Analyse",key='13'):
-#             stop_words=set(stopwords.words("english"))
-#             word_tokens=word_tokenize(message)
-#             filtered_sentence = [w for w in word_tokens if not w in stop_words]  
-#             filtered_sentence = []  
-#             for w in word_tokens:  
-#                 if w not in stop_words:  
-#                     filtered_sentence.append(w)  
-              
-#             st.write(word_tokens)  
-#             st.write(filtered_sentence)
-            
-            
-    if st.checkbox("Stemming and Lemmatization "):
-        selection = st.selectbox("Select type:", ("Lemmatizer", "PorterStemmer"))
-        if st.button("Analyse",key="6"):
-            if selection == "Lemmatizer":
-# 		wordnet_lemmatizer=WordNetLemmatizer()
-                tokenization=nltk.word_tokenize(message)
-                for w in tokenization:
-                    st.write("Lemma for {} is {}".format(w,wordnet_lemmatizer.lemmatize(w))) 
-#                     wordnet_lemmatizer=WordNetLemmatizer()
-	                     
-	  
-            elif selection == "PorterStemmer":
-                porter_stemmer=PorterStemmer()
-                tokenization=nltk.word_tokenize(message)
-                for w in tokenization:
-                    st.write("Stemming for {} is {}".format(w,porter_stemmer.stem(w)))   
-                
-         #hiiiiiiiii
+    if st.checkbox("show words"):         
+        st.write (blob.words)
+    if st.checkbox("show sentence"):    
+        blob.sentences
+    if st.checkbox("lemmatizer"):
+        word1= st.text_area('Enter number:')
+        st.write(Word(word1).lemmatize("v"))
+        st.write(word1.lemmatize("v"))
     if st.checkbox("show text summarization"):
-        if st.button("Analyse",key="7"):
-            st.subheader("summarize your text")
-            summary_result= sumy_summarize(message)
-            st.success(summary_result)
+        st.subheader("summarize your text")
+        message = st.text_area("Enter text ","Type here...")
+        st.text("using summy summarizer")
+        summary_result= sumy_summarize(message)
+        st.success(summary_result)
         
     if st.checkbox("splelling checker"):
-        if st.button("Analyse",key="8"):
-            blob = TextBlob(message)
-            st.write(blob.correct())
-    if st.checkbox("language detector"):
-        if st.button("Analyse",key="15"):
-            blob = TextBlob(message)
-            st.write(blob.detect_language())
+        word1= st.text_area('Enter number:')
+        blob = TextBlob(word1)
+        st.write(blob.correct())
+    if st.checkbox("Translate to German from English"):
+        word1= st.text_area('Enter number:')
+        blob = TextBlob(word1)
+        blob.translate(to="de")
 
-    if st.checkbox("Translate sentences"):
-        selection = st.selectbox("Select language:", ("French", "Spanish","Chinese"))
-	
-        if st.button("Analyse",key="20"):
-            if selection == "French":
-                blob = TextBlob(message)
-                translated=blob.translate(to="fr")
-                st.write(translated)
-                
-            if selection == "Spanish":
-                blob = TextBlob(message)
-                translated=blob.translate(to='es')
-                st.write(translated)
-                
-            if selection == "Chinese":
-                blob = TextBlob(message)
-                translated=blob.translate(to="zh")
-                st.write(translated)    
+
+
