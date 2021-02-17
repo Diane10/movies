a48ca524adb75b5452e5cff421afd9895236e215
Diane10 <46956585+Diane10@users.noreply.github.com>
2021-02-17 22:28:15 +0200

    Update nlp.py

--- nlp.py
+++ nlp.py
@@ -1,181 +1,104 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Feb 15 16:26:01 2021
+
+@author: HP
+"""
+
+import streamlit as st
+#import streamlit.components.v1 as stc
+
 import pandas as pd
-import numpy as np
-from textblob import Word
-from textblob import TextBlob
-import nltk
-import nltk
-nltk.download('punkt')
-import streamlit as stimport nltk
-from nltk.corpus import stopwords
-from nltk.stem import 	WordNetLemmatizer
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.metrics.pairwise import cosine_similarity,linear_kernel
 
-import numpy as np
-import matplotlib.pyplot as plt
-import pandas as pd
-from apyori import apriori
-from mlxtend.frequent_patterns import apriori
-from mlxtend.frequent_patterns import association_rules
-from mlxtend.preprocessing import TransactionEncoder
-from mlxtend.frequent_patterns import apriori, association_rules
+def load_data(data):
+    df= pd.read_csv(data)
+    return df
 
-nltk.download("wordnet")
-nltk.download("brown")
-nltk.download('averaged_perceptron_tagger') 
-from nltk.corpus import wordnet 
-from sumy.parsers.plaintext import PlaintextParser
-from sumy.nlp.tokenizers import Tokenizer
-from sumy.summarizers.lex_rank import LexRankSummarizer
-from nltk.stem.porter import PorterStemmer
-from nltk.stem import WordNetLemmatizer
-#Nlp
+def vectorize_text_to_cosine_max(data):
+    count_vec= CountVectorizer()
+    cv_mat= count_vec.fit_transform(data)
+    cosine_sim=cosine_similarity(cv_mat)
+    return cosine_sim
+def get_recommendation(title,cosine_sim_mat,df,num_of_rec=5):
+    course_indices=pd.Series(df.index,index=df['title']).drop_duplicates()
+    idx=course_indices[title]
+    sim_scores=list(enumerate(cosine_sim_mat[idx]))
+    sim_scores= sorted(sim_scores,key=lambda x:x[1],reverse=True)
+    selected_course_indices=[i[0] for i in sim_scores[1:]]
+    selected_course_score=[i[0] for i in sim_scores[1:]]
+    result_df= df.iloc[selected_course_indices]   
+    result_df['similarity score']=selected_course_score
+    final_recommeded= result_df['title','authors','similarity score','image_url']
+    return final_recommeded
 
-wordnet_lemmatizer=WordNetLemmatizer() 
-def sumy_summarize(docx):
-    parser = PlaintextParser.from_string(docx,Tokenizer("english"))
-    lex_summarizer = LexRankSummarizer()
-    summary = lex_summarizer(parser.document,3)
-    summary_list = [str(sentence) for sentence in summary]
-    result = ' '.join(summary_list)
-    return result
-    
-def pos_tagger(nltk_tag): 
-    if nltk_tag.startswith('J'): 
-        return wordnet.ADJ 
-    elif nltk_tag.startswith('V'): 
-        return wordnet.VERB 
-    elif nltk_tag.startswith('N'): 
-        return wordnet.NOUN 
-    elif nltk_tag.startswith('R'): 
-        return wordnet.ADV 
-    else:           
-        return None    
+@st.cache
+def search_term_if_not_found(term,df):
+    result_df= df[df['title'].str.contains(term)]
+    return result_df
 
-if st.sidebar.checkbox("Market Basket Anlysis"):
-    st.title("Market Basket Anlysis with Streamlit")	
-    dataset = pd.read_csv('https://raw.githubusercontent.com/Diane10/movies/main/GroceryStoreDataSet.csv')
-    dataset = list(dataset["Transaction"].apply(lambda x:x.split(',')))
-    st.write(dataset)
-    te = TransactionEncoder()
-    te_data = te.fit(dataset).transform(dataset)
-    df = pd.DataFrame(te_data,columns=te.columns_)
-    df = df.replace(True,1) 
-    df = df.astype(int)
-    st.write(df)
-    
-    freq_items = apriori(df,min_support=0.10,use_colnames=True) # Support Values
-    
-    st.write(freq_items.sort_values(by = "support" , ascending = False))
-    
-    rules = association_rules(freq_items, metric="lift", min_threshold=1)
-    st.write(rules)
-    
-    df1 = association_rules(freq_items, metric = "confidence", min_threshold = 0.02)
-    
-    st.write(df1)
-    
-    st.write(df1[(df1.confidence > 0.8) & (df1.lift > 1)].sort_values(by="lift", ascending=False))    
-    
+#css style
 
-if st.sidebar.checkbox("NLP"):
-    st.title("Natural Language Processing with Streamlit")
-    message =st.text_area("Enter text")
-    blob = TextBlob(message)
-    if st.checkbox('Noun phrases'):
-        if st.button("Analyse",key="1"):
-#              text1 =st.text_area("Enter text")
-             blob = TextBlob(message)
-             st.write(blob.noun_phrases)
-    if st.checkbox("show sentiment analysis"):
-#         st.subheader("analyse your text")
-#         message=st.text_area("Enter your text")  
-        if st.button("Analyse",key="2"):
-            blob = TextBlob(message)
-            result_sentiment= blob.sentiment
-            st.success(result_sentiment)
-            polarity = blob.polarity
-            subjectivity = blob.subjectivity
-            st.write(polarity, subjectivity)
-    if st.checkbox("show words"): 
-        if st.button("Analyse",key="3"):
-            blob = TextBlob(message)
-            st.write (blob.words)
-    if st.checkbox("show sentence"):
-        if st.button("Analyse",key='30'):
-            blob = TextBlob(message)
-            st.write(blob.sentences)
-    if st.checkbox("Tokenize sentence"): 
-        if st.button("Analyse",key='27'):
-            list2 = nltk.word_tokenize(message) 
-            st.write(list2) 
-    if st.checkbox("POS tag "): 
-        if st.button("Analyse",key='20'):
-            pos_tagged = nltk.pos_tag(nltk.word_tokenize(message))   
-            st.write(pos_tagged) 
+st.title("Book Recommendation APP")
+menu= ["Home","Recommender","About"]
+choice=st.sidebar.selectbox("Menu",menu)
+df=load_data("https://raw.githubusercontent.com/sahilpocker/Book-Recommender-System/master/Dataset/books.csv")
+df=df[:200]
+if choice=='Home':
+    st.subheader("Home")
+    st.dataframe(df.head(10))
+    
+elif choice =="Recommender":
+    st.subheader("Recommend Book")
+    cosine_sim_mat=vectorize_text_to_cosine_max(df['title'])
+    search_term=st.text_input("Search Boook")
+    num_of_rec= st.sidebar.number_input("Number",4,30,7)
+    if st.button("Recommend"):
+        if search_term is not None:
+            try:
+                result= get_recommendation(search_term,cosine_sim_mat,df,num_of_rec)
+                for row in result.iterrows():
+                    rec_title= row[1][0]
+                    rec_author= row[1][1]
+                    rec_score= row[1][2]
+                    rec_image= row[1][3]
+                    
+                    c1,c2,c3 = st.beta_columns([1,2,1])
+                    with c1:
+                        with st.beta_expander("Title"):
+                            st.success(rec_title)
+                            
+                    with c2:
+                        with st.beta_expander("image"):
+                            st.image(rec_image,use_column_with=True) 
+                            
+                            
+                    with c1:
+                        with st.beta_expander("author"):
+                            st.success(rec_author)        
+
+                    
+                    #st.write("Title",rec_title)
+                    #st.write("author",rec_author)
+                    #st.write("Title",rec_score)
+                    #st.image(rec_image)
+                    
+                    
+                    
+            except: 
+                st.warning('Book Not Found')
+                st.info("suggested Option include")
+                result_df=search_term_if_not_found(search_term,df)
+                st.dataframe(result_df)
             
-    if st.checkbox("Removing stopword"): 
-
-        if st.button("Analyse",key='13'):
-            stop_words=set(stopwords.words("english"))
-            word_tokens=word_tokenize(message)
-            filtered_sentence = [w for w in word_tokens if not w in stop_words]  
-            filtered_sentence = []  
-            for w in word_tokens:  
-                if w not in stop_words:  
-                    filtered_sentence.append(w)  
-              
-            st.write(word_tokens)  
-            st.write(filtered_sentence)
             
-           
-    if st.checkbox("Text preprocessing"):
-        selection = st.selectbox("Select type:", ("Lemmatizer", "PorterStemmer"))
-        if st.button("Analyse",key="4"):
-            if selection == "Lemmatizer":
                 
-                tokenization=nltk.word_tokenize(message)
-		
-                for w in tokenization:
-		     	
-                    st.write("Lemma for {} is {}".format(w,wordnet_lemmatizer.lemmatize(w))) 
-                               
-	  
-            elif selection == "PorterStemmer":
-                porter_stemmer=PorterStemmer()
-                tokenization=nltk.word_tokenize(message)
-                for w in tokenization:
-                    st.write("Stemming for {} is {}".format(w,porter_stemmer.stem(w)))   
-                
-              
-    if st.checkbox("show text summarization"):
-        if st.button("Analyse",key="5"):
-            st.subheader("summarize your text")
-            summary_result= sumy_summarize(message)
-            st.success(summary_result)
-        
-    if st.checkbox("splelling checker"):
-        if st.button("Analyse",key="6"):
-            blob = TextBlob(message)
-            st.write(blob.correct())
-    if st.checkbox("language detector"):
-        if st.button("Analyse",key="15"):
-            blob = TextBlob(message)
-            st.write(blob.detect_language())
-
-    if st.checkbox("Translate sentences"):
-        selection = st.selectbox("Select language:", ("French", "Spanish","Chinese"))
-	
-        if st.button("Analyse",key='23'):
-            if selection == "French":
-                blob = TextBlob(message)
-                translated=blob.translate(to="fr")
-                st.write(translated)
-                
-            if selection == "Spanish":
-                blob = TextBlob(message)
-                translated=blob.translate(to='es')
-                st.write(translated)
-#                 
-            if selection == "Chinese":
-                blob = TextBlob(message)
-                translated=blob.translate(to="zh")
-                st.write(translated)    
+else:
+    st.subheader("About US")
+    
+    
+    
+    
+    
+   
